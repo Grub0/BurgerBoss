c38e8b58c145abf4c408a70b63d3212e65d54f10
Grub0 <rjc9008@rit.edu>
2015-01-22 02:27:35 -0500

    Tweaks

--- Assets/Scripts/Controllers/enemyController.cs
+++ Assets/Scripts/Controllers/enemyController.cs
@@ -14,7 +14,10 @@ public class enemyController : MonoBehaviour {
 	// Use this for initialization
 	void Start () 
 	{
-	
+		if(player == null)
+		{
+			player = GameObject.FindGameObjectWithTag("Player");
+		}
 	}
 	
 	// Update is called once per frame
